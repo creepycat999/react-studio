8e25d99fd2f67797f95d4762566541f5a2f0a5a5
@f4sxkdn1kp <90721727+f4sxkdn1kp@users.noreply.github.com>
2022-11-29 13:32:55 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -8,22 +9,44 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+function total(count, setCount, item){
+  setCount(count + (item.price))
+}
+function addBaked(cart, setCart, item){
+  setCart(cart + (item.name) + ", ")
+}
+
 function App() {
+
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState([]);
+  //const listItems = cart.map((d) => <li key={d.name}></li>);
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
+      <h1>Baked Bakery</h1>
+      <div ClassName = "outerFlex">
+        <div ClassName="bakedItem">
+        {/* TODO: personalize your bakery (if you want) */}
+        {bakeryData.map((item, index) => 
+        ( // TODO: map bakeryData to BakeryItem components
+          [<p>{BakeryItem(item)} </p>, // replace with BakeryItem component
+          <button onClick={() => {
+            total(count, setCount, item); 
+            addBaked(cart, setCart, item);
+          }}>Add to Cart</button>] ))}
+        
       <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <h2>Cart:</h2>
+        <p>{cart}</p>
+        <div>
+			    <h2>Total: ${count}</h2>
+		    </div>
       </div>
+      </div>
+    </div>
     </div>
   );
 }
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,22 @@
 // TODO: create a component that displays a single bakery item
+
+
+  export default function BakeryItem(props) {
+    return(
+        <div ClassName = "bakedItem">
+            <div ClassName = "outerFlex">
+        <div>
+        <h3>{props.name}</h3>
+        <img
+          src={props.image}
+          alt={props.description}
+          width = "100"
+          height = "100"
+          ></img>
+          <p>{props.description}</p>
+          <p>{props.price}</p>
+        </div>
+        </div>
+        </div>
+    )
+}
